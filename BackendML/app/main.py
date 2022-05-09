2592a0930dc8cd074ebe03862ffc7ef71e75cce7
ibnzterrell <terrell.ibanez@gmail.com>
2022-05-09 15:32:21 -0700

    Rewrite ML prediction as a single call

--- BackendML/app/main.py
+++ BackendML/app/main.py
@@ -5,42 +5,24 @@ from BeatNet.BeatNet import BeatNet
 import struct
 from utils import *
 
-estimator = BeatNet(1, mode='online', inference_model='DBN', plot=[], thread=False)
+estimator = BeatNet(1, mode='offline', inference_model='DBN', plot=[], thread=False)
 
 app = FastAPI()
 
-@app.post("/hello")
-async def hello():
-    return {"msg": "hello!"}
+@app.get("/ping")
+async def ping():
+    return {"message": "pong!"}
     
-@app.post("/files/")
-async def create_file(file: bytes = File(...)):
+@app.post("/upload_test/")
+async def file_test(file: bytes = File(...)):
     return {"file_size": len(file)}
 
-@app.post("/upload")
-async def upload(file: UploadFile = File(...)):
-    try:
-        contents = await file.read()
-        with open(file.filename, 'wb') as f:
-            f.write(contents)
-        packedData = map(lambda v: struct.pack('h', v), contents)
-        frames = b''.join(packedData)
-        output_wave('./assets/example.wav', frames)
-
-
-    except Exception:
-        return {"message": "There was an error uploading the file"}
-    finally:
-        await file.close()
-
-    # return dbn_pred
-@app.get("/predict")
-async def predict():
-    try:
-        dbn_pred = await estimator.process("./assets/example.wav")
-        return dbn_pred
-    except Exception:
-        return {"message":"Check input file"}
+@app.post("/predict")
+async def predict(file: bytes = File(...)):
+    with open("./assets/example.wav", "wb") as f:
+        f.write(file)
+    dbn_pred = estimator.process("./assets/example.wav")
+    return dbn_pred
 
 
 if __name__ == '__main__':
